012bd01fe5a87aff6a37c5d27a23a3c88582623d
Pkoobz <kristoffen.p@gmail.com>
2023-06-13 23:00:51 +0700

    ok

--- Coba1/src/map.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Map from './place.jsx';
-
-function renderMap() {
-  ReactDOM.render(<Map />, document.getElementById('map'));
-}
-
-renderMap();
--- src(2)/components/App.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import Card from "./Card";
-import infoguide from "";
-import "./style.css";
-
